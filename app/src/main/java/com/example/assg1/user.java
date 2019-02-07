cc0b6f7ad6851331c5625b52c1c597d95ed3cfe7
emeri <bmesi002@odu.edu>
2019-02-07 17:59:05 -0500

    user

--- app/src/main/java/com/example/assg1/user.java
+++ app/src/main/java/com/example/assg1/user.java
@@ -3,7 +3,19 @@ package com.example.assg1;
 public class user {
     public String task;
     public String description;
+    private boolean checked = false;
 
+    public boolean isChecked(){
+        return checked;
+    }
+
+    public void toggleChecked(){
+        checked = !checked;
+    }
+
+    public void setChecked(boolean checked){
+        this.checked = checked;
+    }
     public user(String task, String description){
         this.task =  task;
         this.description = description;
