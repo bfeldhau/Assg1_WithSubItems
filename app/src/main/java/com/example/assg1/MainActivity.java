fa969f00fb510806ffa905b98cad4bd8e94d9526
emeri <bmesi002@odu.edu>
2019-02-07 18:00:48 -0500

    mainActivity

--- app/src/main/java/com/example/assg1/MainActivity.java
+++ app/src/main/java/com/example/assg1/MainActivity.java
@@ -10,6 +10,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
@@ -18,6 +19,9 @@ import android.widget.TextView;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -34,8 +38,8 @@ public class MainActivity extends AppCompatActivity {
     ListView listView;
     ArrayList<user> arrayOfTask;
     userAdapter adapter;
-
-
+    CheckBox checkBox;
+    Button btnDel;
 
 
     @Override
@@ -44,14 +48,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         loadData();
-    //test
+        //test
         if (arrayOfTask == null){
             createlist();
         }
 
+        btnDel = (Button) findViewById(R.id.delete);
         btnAdd = (Button) findViewById(R.id.button);
         taskText = (EditText) findViewById(R.id.taskText);
         descriptionText = (EditText) findViewById(R.id.descriptionText);
+        checkBox = (CheckBox) findViewById(R.id.checkbox);
 
 
         adapter = new userAdapter(this, arrayOfTask);
@@ -68,49 +74,74 @@ public class MainActivity extends AppCompatActivity {
                 String descrip = descriptionText.getText().toString();
 
                 newTask = new user(task, descrip);
-                //test
                 adapter.add(newTask);
                 adapter.notifyDataSetChanged();
+                taskText.getText().clear();
+                descriptionText.getText().clear();
                 saveData();
-
+                saveOut();
             }
         });
+
+        //****end of btnAdd***\\\
 
         listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+
                 arrayOfTask.remove(position);
                 adapter.notifyDataSetChanged();
                 saveData();
                 return true;
+
             }
         });
 
-
 /// end of on create
-        }
-
-        private void saveData(){
-            SharedPreferences sharedPreferences = getSharedPreferences("shared preferences", MODE_PRIVATE);
-            SharedPreferences.Editor editor = sharedPreferences.edit();
-            Gson gson = new Gson();
-            String json = gson.toJson(arrayOfTask);
-            editor.putString("task list", json);
-            editor.apply();
-        }
-
-        private void loadData(){
-            SharedPreferences sharedPreferences = getSharedPreferences("shared preferences", MODE_PRIVATE);
-            Gson gson = new Gson();
-            String json = sharedPreferences.getString("task list", null);
-            Type type = new TypeToken<ArrayList<user>>() {}.getType();
-            arrayOfTask = gson.fromJson(json, type);
-
-        }
-
-        private void createlist(){
-            arrayOfTask = new ArrayList<user>();
-        }
-        ///this is end of main activity
     }
 
+
+
+    private void saveData(){
+        SharedPreferences sharedPreferences = getSharedPreferences("shared preferences", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        Gson gson = new Gson();
+        String json = gson.toJson(arrayOfTask);
+        editor.putString("task list", json);
+        editor.apply();
+    }
+
+    private void loadData(){
+        SharedPreferences sharedPreferences = getSharedPreferences("shared preferences", MODE_PRIVATE);
+        Gson gson = new Gson();
+        String json = sharedPreferences.getString("task list", null);
+        Type type = new TypeToken<ArrayList<user>>() {}.getType();
+        arrayOfTask = gson.fromJson(json, type);
+
+    }
+
+    private void createlist(){
+        arrayOfTask = new ArrayList<user>();
+    }
+
+    public void saveOut(){
+        String FileName = "results.txt";
+        Gson gson = new Gson();
+        String json = gson.toJson(arrayOfTask);
+        //test
+        FileOutputStream fos = null;
+        try {
+            fos = openFileOutput(FileName, Context.MODE_PRIVATE);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            fos.write(json.getBytes());
+            fos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    ///this is end of main activity
+}
